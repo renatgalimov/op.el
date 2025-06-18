af2f420e52dac1d3047ccee782184e4bac01532c
Renat Galimov <13913521+renatgalimov@users.noreply.github.com>
2025-06-18 17:48:15 +0300

    Fix Cask install step

--- test/op-test.el
+++ test/op-test.el
@@ -1,24 +1,28 @@
-(require 'ert)
+;;; op-test.el --- unit tests -*- lexical-binding: t; -*-
+
+(require 'buttercup)
 (load-file "op.el")
 
-(ert-deftest op-read-caches-result ()
-  (let ((op-read-cache (make-hash-table :test 'equal))
-        (called 0))
-    (cl-letf (((symbol-function 'shell-command-to-string)
-               (lambda (&rest _)
-                 (setq called (1+ called))
-                 "secret")))
-      (should (string= (op-read "item") "secret"))
-      (should (= called 1))
-      (should (string= (op-read "item") "secret"))
-      (should (= called 1)))))
+(describe "op-read"
+	  (it "when called twice should use the cached result"
+	      (let ((op-read-cache (make-hash-table :test 'equal))
+		    (called 0))
+		(spy-on 'shell-command-to-string
+			:and-call-fake (lambda (&rest _)
+					 (setq called (1+ called))
+					 "secret"))
+		(expect (op-read "item") :to-equal "secret")
+		(expect called :to-equal 1)
+		(expect (op-read "item") :to-equal "secret")
+		(expect called :to-equal 1))))
 
-(ert-deftest op-read-cache-cleanup-removes-old-entries ()
-  (let ((op-read-cache (make-hash-table :test 'equal)))
-    (puthash "old" (cons "val" (- (float-time) (* 11 60))) op-read-cache)
-    (puthash "recent" (cons "val" (float-time)) op-read-cache)
-    (op-read-cache-cleanup)
-    (should-not (gethash "old" op-read-cache))
-    (should (gethash "recent" op-read-cache))))
+(describe "op-read-cache-cleanup"
+	  (it "when run should remove expired entries"
+	      (let ((op-read-cache (make-hash-table :test 'equal)))
+		(puthash "old" (cons "val" (- (float-time) (* 11 60))) op-read-cache)
+		(puthash "recent" (cons "val" (float-time)) op-read-cache)
+		(op-read-cache-cleanup)
+		(expect (gethash "old" op-read-cache) :to-be nil)
+		(expect (gethash "recent" op-read-cache) :not :to-be nil))))
 
 (provide 'op-test)
--- test/run-tests.el
+++ test/run-tests.el
@@ -1,6 +1,10 @@
+;;; run-tests.el --- run Buttercup suite -*- lexical-binding: t; -*-
+
 (let* ((script-dir (file-name-directory (or load-file-name buffer-file-name)))
        (root-dir (expand-file-name ".." script-dir)))
   (add-to-list 'load-path root-dir)
+  (add-to-list 'load-path script-dir)
+  (require 'buttercup)
   (load (expand-file-name "op-test.el" script-dir)))
 
-(ert-run-tests-batch-and-exit)
+(buttercup-run)
